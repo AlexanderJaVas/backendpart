5db69f86aef23bfcd8c530d60aaf873cf487e219
Dss <sd;fjkwoiefjsldkfj@adjskjd.org>
2023-08-16 15:30:02 +0200

    Initial commit

---
+++ src/main/java/com/example/backendpart/BackendpartApplication.java
@@ -0,0 +1,13 @@
+package com.example.backendpart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendpartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendpartApplication.class, args);
+	}
+
+}
